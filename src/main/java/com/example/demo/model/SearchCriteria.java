79efd8d7b009bf2747481dc5a0970483762e7a83
TaiDyou@example.com <dttai1800732@student.ctuet.edu.vn>
2022-04-19 00:46:35 +0700

    upload

---
+++ src/main/java/com/example/demo/model/SearchCriteria.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+
+public class SearchCriteria {
+
+    
+    String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
